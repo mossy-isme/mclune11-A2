f5989be50847c59c0c6498c689a7517c05f8964d
mossy-isme <mossycluney@gmail.com>
2024-11-27 17:59:04 +1100

    modified:   src/Person.java - added setters, getters and constructors

--- src/AssignmentTwo.java
+++ src/AssignmentTwo.java
@@ -8,5 +8,5 @@
  * @author mosscluney
  */
 public class AssignmentTwo {
-
+    
 }
--- src/Person.java
+++ src/Person.java
@@ -8,5 +8,40 @@
  * @author mosscluney
  */
 public class Person {
+    private int ID;
+    private String name;
+    private String DOB;
+    
+    public Person() {
+    }
 
+    public Person(int ID, String name, String DOB) {
+        this.ID = ID;
+        this.name = name;
+        this.DOB = DOB;
+    }
+
+    public int getID() {
+        return this.ID;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDOB() {
+        return this.DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+
+    
+
+    
 }
