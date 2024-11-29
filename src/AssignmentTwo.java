0e5c1c3ab6af0dde280198ebd7bba63164bef2c0
mossy-isme <mossycluney@gmail.com>
2024-11-29 19:07:45 +1100

    modified:   src/AssignmentTwo.java

--- src/AssignmentTwo.java
+++ src/AssignmentTwo.java
@@ -149,7 +149,12 @@ public class AssignmentTwo {
 
     public static void main(String[] args) {
         AssignmentTwo assignmentTwo = new AssignmentTwo();
-        assignmentTwo.partSeven();
+        assignmentTwo.partThree();
+        //assignmentTwo.partFourA();
+        //assignmentTwo.partFourB();
+        //assignmentTwo.partFive();
+        //assignmentTwo.partSix();
+        //assignmentTwo.partSeven();
     }
     
 }
